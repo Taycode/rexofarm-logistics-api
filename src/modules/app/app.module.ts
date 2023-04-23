60f4071564e4eb1be0749d1e1230ffe88aaf37d0
Abdulmateen Tairu <tay2druh@gmail.com>
2023-04-23 13:36:54 +0100

    feat: init app

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,62 @@
+import { Module } from '@nestjs/common';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+import V1Module from '@v1/v1.module';
+
+import AppController from './app.controller';
+import AppService from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL as string, {
+      autoReconnect: true,
+      useCreateIndex: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    RedisModule.forRootAsync({
+      useFactory: (cfg: ConfigService) => ({
+        config: {
+          url: cfg.get('REDIS_URL'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    MailerModule.forRootAsync({
+      useFactory: (cfg: ConfigService) => ({
+        transport: {
+          host: cfg.get('MAILER_HOST'),
+          port: Number(cfg.get('MAILER_PORT')),
+          secure: false,
+          auth: {
+            user: cfg.get('MAILER_USERNAME'),
+            pass: cfg.get('MAILER_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: cfg.get('MAILER_FROM_EMAIL'),
+        },
+        template: {
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    V1Module,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export default class AppModule {}
