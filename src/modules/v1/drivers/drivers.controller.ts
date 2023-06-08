484f802896121c6f339219c73d1b817359fafa26
Abdulmateen Tairu <tay2druh@gmail.com>
2023-06-08 20:46:48 +0100

    feat: kyc complete

--- src/modules/v1/drivers/drivers.controller.ts
+++ src/modules/v1/drivers/drivers.controller.ts
@@ -1,9 +1,10 @@
 import {
-  Body, Controller, HttpException, HttpStatus, Patch, Req,
+  Body, Controller, HttpException, HttpStatus, Patch, Req, UseGuards,
 } from '@nestjs/common';
 import { DriversService } from '@v1/drivers/drivers.service';
 import { UpdateDriverCtrlDto, UpdateDriverNextOfKinCtrlDto } from '@v1/drivers/dto/controller/update-driver-ctrl.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JWTAuthGuard } from '@v1/auth/guards/jwt.guard';
 import { CustomRequest } from '../../../types/request.type';
 
 @Controller('drivers')
@@ -13,6 +14,8 @@ export class DriversController {
     private readonly driversService: DriversService,
   ) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JWTAuthGuard)
   @Patch('profile')
   async updateDriverProfile(@Body() updatePayload: UpdateDriverCtrlDto, @Req() req: CustomRequest) {
     const { user } = req;
@@ -21,6 +24,8 @@ export class DriversController {
     return { message: 'Driver updated successfully', data: updatedDriver };
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JWTAuthGuard)
   @Patch('next-of-kin')
   async updateDriverNextOfKin(@Body() updatePayload: UpdateDriverNextOfKinCtrlDto, @Req() req: CustomRequest) {
     const { user } = req;
--- src/modules/v1/kyc/kyc.controller.ts
+++ src/modules/v1/kyc/kyc.controller.ts
@@ -1,20 +1,46 @@
 import { KYCService } from '@v1/kyc/kyc.service';
 import {
-  Controller, Post, Req, UploadedFile,
+  Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors,
 } from '@nestjs/common';
 import type { Express } from 'express';
+import {
+  ApiBearerAuth, ApiBody, ApiConsumes, ApiTags,
+} from '@nestjs/swagger';
+import { JWTAuthGuard } from '@v1/auth/guards/jwt.guard';
+import { ApplyForKycDto } from '@v1/kyc/dto/controller/apply-for-kyc.dto';
 import { CustomRequest } from '../../../types/request.type';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
-@Controller()
+@Controller('kyc')
+@ApiTags('KYC')
 export class KycController {
   constructor(private readonly kycService: KYCService) {}
 
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: memoryStorage(),
+    }),
+  )
+  @ApiBearerAuth()
+  @UseGuards(JWTAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: ApplyForKycDto })
   @Post('nin')
   async applyForNin(@UploadedFile() file: Express.Multer.File, @Req() req: CustomRequest) {
     const { user } = req;
     return this.kycService.applyForNin(file, user);
   }
 
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: memoryStorage(),
+    }),
+  )
+  @ApiBearerAuth()
+  @UseGuards(JWTAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: ApplyForKycDto })
   @Post('driver-license')
   async applyForDriverLicense(@UploadedFile() file: Express.Multer.File, @Req() req: CustomRequest) {
     const { user } = req;
